e5ffa7db8fd996f48a097e7d915f181e8679e7df
Sergey P <sergeyp@4ib.com>
2024-04-23 16:54:15 +0300

    vnvn

---
+++ WebApi/Controllers/WarehouseController.cs
@@ -0,0 +1,46 @@
+﻿using SkladEngine.DBFunction;
+using SkladEngine.ExecuteDoc;
+using SP.Base;
+using SP.Base.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi.Controllers.Models;
+using WebApi.Core;
+
+
+namespace WebApi.Controllers
+{
+    [RoutePrefix("api/warehouse")]
+    [ApiTokenAuthorize]
+    public class WarehouseController : BaseApiController
+    {
+        [HttpGet, Route("remain")]
+        public IHttpActionResult GetRemainInWh()
+        {
+            using (var sp_base = SPDatabase.SPBase())
+            {
+                var ka = sp_base.Kagent.FirstOrDefault(w => w.Id == Context.Token);
+
+                return Ok(new MaterialRemain(0).GetMaterialsOnWh(ka.WId.Value));
+            }
+        }
+
+        [HttpGet, Route("add-doc/{wbill_id}")]
+        public void AddDocument(int wbill_id)
+        {
+            var new_doc = new ExecuteWayBill().MoveToStoreWarehouse(wbill_id, true);
+            if (!new_doc.HasValue)
+            {
+                //"Прибуткова накладана вже стовена!";
+            }
+            
+        }
+
+    }
+
+
+}
---
+++ WebApi/Controllers/WaybillInController.cs
@@ -0,0 +1,30 @@
+﻿using SkladEngine.DBFunction;
+using SkladEngine.ExecuteDoc;
+using SP.Base;
+using SP.Base.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi.Controllers.Models;
+using WebApi.Core;
+
+namespace WebApi.Controllers
+{
+    [RoutePrefix("api/waybill-in")]
+    [ApiTokenAuthorize]
+    public class WaybillInController : BaseApiController
+    {
+        [HttpGet, Route("{wbill_id}/det")]
+        public IHttpActionResult GetWaybilldet(int wbill_id)
+        {
+            using (var sp_base = SPDatabase.SPBase())
+            {
+                return Ok(sp_base.v_WaybillDet.Where(w=> w.WbillId == wbill_id).ToList());
+            }
+        }
+
+    }
+}
