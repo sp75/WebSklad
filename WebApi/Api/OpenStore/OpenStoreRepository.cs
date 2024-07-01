6e19e50a0c29ac0e2156707c3548d2c2e9084a3a
Sergey P <sergeyp@4ib.com>
2024-07-01 16:25:02 +0300

    rtr

--- WebApi/Api/OpenStore/OpenStoreRepository.cs
+++ WebApi/Api/OpenStore/OpenStoreRepository.cs
@@ -110,5 +110,104 @@ where x.LastInventoryDate is not null", id).FirstOrDefault();
 
             ImportKagentSales(ka.KaId, ka.OpenStoreAreaId.Value, ka.LastInventoryDate.Value, ka.WId.Value);
         }
+
+
+        public void ImportKagentReturns(int ka_id, int area_id, DateTime last_inventory_date, int wid)
+        {
+            using (var sp_base = SPDatabase.SPBase())
+            {
+                var _enterprise = sp_base.Kagent.FirstOrDefault(w => w.KType == 3 && w.Deleted == 0 && (w.Archived == null || w.Archived == 0) && w.EnterpriseWorker.Any(a => a.WorkerId == ka_id));
+
+                var ka_sales_out = sp_base.Database.SqlQuery<SalesList>(@"SELECT 
+  v_ReturnSales.ARTID 
+ ,v_ReturnSales.ARTCODE
+ ,v_ReturnSales.ARTNAME
+ ,v_ReturnSales.SESSID
+ ,v_ReturnSales.SAREAID
+ ,v_ReturnSales.SYSTEMID
+ ,v_ReturnSales.SessionStartDate
+ ,m.MatId
+ ,SUM(v_ReturnSales.AMOUNT) Amount
+ ,SUM(v_ReturnSales.TOTAL) Total
+FROM [SERVER_OS].[Tranzit_OS].[dbo].v_ReturnSales
+inner join Materials m on m.OpenStoreId = v_ReturnSales.ARTID
+left outer join  [SERVER_OS].[Tranzit_OS].[dbo].SESS_EXPOTR on SESS_EXPOTR.SESSID = v_ReturnSales.SESSID and SESS_EXPOTR.SYSTEMID = v_ReturnSales.SYSTEMID and SESS_EXPOTR.SAREAID = v_ReturnSales.SAREAID
+WHERE SESSEND IS NOT null and  v_ReturnSales.SAREAID = {0} AND coalesce( m.Archived,0) = 0 and SessionStartDate > {1} and  SESS_EXPOTR.SYSTEMID is null and m.TypeId in (1,5)
+GROUP BY v_ReturnSales.SESSID, v_ReturnSales.SAREAID, ARTID, ARTCODE, ARTNAME, SessionStartDate, v_ReturnSales.[SYSTEMID], m.MatId", area_id, last_inventory_date).ToList();
+
+                foreach (var mat_sales_item in ka_sales_out.GroupBy(g => new { g.SESSID, g.SYSTEMID, g.SessionStartDate, g.SAREAID }).ToList())
+                {
+                    var wb = sp_base.WaybillList.Add(new WaybillList()
+                    {
+                        Id = Guid.NewGuid(),
+                        WType = 5,
+                        DefNum = 0,
+                        OnDate = DateTime.Now,
+                        Num = sp_base.GetDocNum("wb_write_off").FirstOrDefault(),
+                        CurrId = 2,
+                        OnValue = 1,
+                        //   PersonId = DBHelper.CurrentUser.KaId,
+                   //     WaybillMove = new WaybillMove { SourceWid = wid },
+                        Nds = 0,
+                        //       UpdatedBy = DBHelper.CurrentUser.UserId,
+                        EntId = _enterprise?.KaId,
+                        AdditionalDocTypeId = 3, //Повернення
+                        Reason = $"Почток змніни: {mat_sales_item.Key.SessionStartDate}, Номер каси: {mat_sales_item.Key.SYSTEMID}",
+                        Notes = $"Повернення товарів за зміну по касі №{mat_sales_item.Key.SYSTEMID}"
+                    });
+
+                    sp_base.SaveChanges();
+
+                    using (var tr_os_db = new Tranzit_OSEntities())
+                    {
+                        tr_os_db.SESS_EXPOTR.Add(new SESS_EXPOTR { SAREAID = mat_sales_item.Key.SAREAID, SESSID = mat_sales_item.Key.SESSID, SYSTEMID = mat_sales_item.Key.SYSTEMID });
+                        tr_os_db.SaveChanges();
+                    }
+
+                    foreach (var item in mat_sales_item.ToList())
+                    {
+                        var wbd = sp_base.WaybillDet.Add(new WaybillDet()
+                        {
+                            WbillId = wb.WbillId,
+                            Num = wb.WaybillDet.Count() + 1,
+                            Amount = item.Amount,
+                            OnValue = wb.OnValue,
+                            WId = wid,
+                            Nds = wb.Nds,
+                            CurrId = wb.CurrId,
+                            OnDate = wb.OnDate,
+                            MatId = item.MatId,
+                            Price = item.Total / item.Amount,
+                            BasePrice = item.Total / item.Amount
+                        });
+                    }
+
+                    sp_base.SaveChanges();
+
+                    wb.UpdatedAt = DateTime.Now;
+
+                    sp_base.SaveChanges();
+
+
+                    foreach (var det_item in db.WaybillDet.Where(w => w.WbillId == wb.WbillId).ToList())
+                    {
+                        db.WMatTurn.Add(new WMatTurn
+                        {
+                            PosId = det_item.PosId,
+                            WId = det_item.WId.Value,
+                            MatId = det_item.MatId,
+                            OnDate = det_item.OnDate.Value,
+                            TurnType = 1,
+                            Amount = det_item.Amount,
+                            SourceId = det_item.PosId
+                        });
+                    }
+
+                    wb.Checked = 1;
+
+                    db.SaveChanges();
+                }
+            }
+        }
     }
 }
